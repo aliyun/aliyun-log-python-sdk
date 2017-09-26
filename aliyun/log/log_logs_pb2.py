6adfaeae68edc78bfaacfaacfb3c976db0ad4d8d
tingtao.suntt <tingtao.suntt@alibaba-inc.com>
2017-09-26 15:22:36 +0800

    fix proto typo error

--- aliyun/log/log_logs_pb2.py
+++ aliyun/log/log_logs_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='log_logs.proto',
   package='',
-  serialized_pb='\n\x0elog_logs.proto\"Z\n\x03Log\x12\x0c\n\x04Time\x18\x01 \x02(\r\x12\x1e\n\x08\x43ontents\x18\x02 \x03(\x0b\x32\x0c.Log.Content\x1a%\n\x07\x43ontent\x12\x0b\n\x03Key\x18\x01 \x02(\t\x12\r\n\x05Value\x18\x02 \x02(\t\"$\n\x06LogTag\x12\x0b\n\x03Key\x18\x01 \x02(\t\x12\r\n\x05Value\x18\x02 \x02(\t\"~\n\x08LogGroup\x12\x12\n\x04Logs\x18\x01 \x03(\x0b\x32\x04.Log\x12\x10\n\x08Reserved\x18\x02 \x01(\t\x12\r\n\x05Topic\x18\x03 \x01(\t\x12\x0e\n\x06Source\x18\x04 \x01(\t\x12\x13\n\x0bMachineUUID\x18\x05 \x01(\t\x12\x18\n\x07LogTags\x18\x06 \x03(\x0b\x32\x07.LogTag\"/\n\x0cLogGroupList\x12\x1f\n\x0clogGroupList\x18\x01 \x03(\x0b\x32\t.LogGroup')
+  serialized_pb='\n\x0elog_logs.proto\"Z\n\x03Log\x12\x0c\n\x04Time\x18\x01 \x02(\r\x12\x1e\n\x08\x43ontents\x18\x02 \x03(\x0b\x32\x0c.Log.Content\x1a%\n\x07\x43ontent\x12\x0b\n\x03Key\x18\x01 \x02(\t\x12\r\n\x05Value\x18\x02 \x02(\t\"$\n\x06LogTag\x12\x0b\n\x03Key\x18\x01 \x02(\t\x12\r\n\x05Value\x18\x02 \x02(\t\"~\n\x08LogGroup\x12\x12\n\x04Logs\x18\x01 \x03(\x0b\x32\x04.Log\x12\x10\n\x08Reserved\x18\x02 \x01(\t\x12\r\n\x05Topic\x18\x03 \x01(\t\x12\x0e\n\x06Source\x18\x04 \x01(\t\x12\x13\n\x0bMachineUUID\x18\x05 \x01(\t\x12\x18\n\x07LogTags\x18\x06 \x03(\x0b\x32\x07.LogTag\",\n\x0cLogGroupList\x12\x1c\n\tLogGroups\x18\x01 \x03(\x0b\x32\t.LogGroup')
 
 
 
@@ -193,7 +193,7 @@ _LOGGROUPLIST = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='logGroupList', full_name='LogGroupList.logGroupList', index=0,
+      name='LogGroups', full_name='LogGroupList.LogGroups', index=0,
       number=1, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
@@ -209,14 +209,14 @@ _LOGGROUPLIST = _descriptor.Descriptor(
   is_extendable=False,
   extension_ranges=[],
   serialized_start=276,
-  serialized_end=323,
+  serialized_end=320,
 )
 
 _LOG_CONTENT.containing_type = _LOG;
 _LOG.fields_by_name['Contents'].message_type = _LOG_CONTENT
 _LOGGROUP.fields_by_name['Logs'].message_type = _LOG
 _LOGGROUP.fields_by_name['LogTags'].message_type = _LOGTAG
-_LOGGROUPLIST.fields_by_name['logGroupList'].message_type = _LOGGROUP
+_LOGGROUPLIST.fields_by_name['LogGroups'].message_type = _LOGGROUP
 DESCRIPTOR.message_types_by_name['Log'] = _LOG
 DESCRIPTOR.message_types_by_name['LogTag'] = _LOGTAG
 DESCRIPTOR.message_types_by_name['LogGroup'] = _LOGGROUP
