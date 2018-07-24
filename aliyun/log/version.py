c1f220812b1bd0c541ac916c4dbe4a154da93110
wjo1212 <wjo1212@163.com>
2018-07-24 14:40:14 +0800

    skip external test case due to env issue

--- aliyun/log/version.py
+++ aliyun/log/version.py
@@ -1,4 +1,4 @@
-__version__ = '0.6.30.4'
+__version__ = '0.6.30.5'
 USER_AGENT = 'log-python-sdk-v-' + __version__
 LOGGING_HANDLER_USER_AGENT = 'logging-handler, ' + USER_AGENT
 API_VERSION = '0.6.0'
