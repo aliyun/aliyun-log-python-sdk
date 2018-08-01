0a6a5a5b7defe677b071c7f244e4f3f369c28f44
brucewu-fly <brucewu.fly.cn@gmail.com>
2018-08-01 19:32:07 +0800

    Update version from 0.6.31 to 0.6.31.1

--- aliyun/log/version.py
+++ aliyun/log/version.py
@@ -1,4 +1,4 @@
-__version__ = '0.6.31'
+__version__ = '0.6.31.1'
 USER_AGENT = 'log-python-sdk-v-' + __version__
 LOGGING_HANDLER_USER_AGENT = 'logging-handler, ' + USER_AGENT
 ES_MIGRATION_USER_AGENT = 'es-migration, ' + USER_AGENT
