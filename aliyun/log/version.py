5fbe5a0d8034a36ffd298168ad9ca0fa0ec2513f
wjo1212 <wjo1212@163.com>
2018-08-22 14:53:05 +0800

    upgrade to v0.6.33

--- aliyun/log/version.py
+++ aliyun/log/version.py
@@ -1,4 +1,4 @@
-__version__ = '0.6.32.2'
+__version__ = '0.6.33'
 USER_AGENT = 'log-python-sdk-v-' + __version__
 LOGGING_HANDLER_USER_AGENT = 'logging-handler, ' + USER_AGENT
 ES_MIGRATION_USER_AGENT = 'es-migration, ' + USER_AGENT
