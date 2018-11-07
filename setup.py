c06ca37a965f5e433a3df769a2f956c85905ca08
wjo1212 <wjo1212@163.com>
2018-11-07 20:05:23 +0800

    seems travis CI doesn't support the new setup syntax

--- setup.py
+++ setup.py
@@ -21,30 +21,30 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
 import sys
 import re
 
 if sys.version_info[:2] == (2, 6):
     install_requires = ['requests', 'protobuf <= 3.4.0', 'six', 'enum34', 'futures', 'python-dateutil',
                         'elasticsearch>=6.0.0,<7.0.0']
-else:
-    # this syntax is not supported by python 2.6
-    install_requires = ['requests',
-                        'protobuf <= 3.4.0; python_version <= 2.6',
-                        'protobuf >= 3.4.0; python_version >= 2.7',
-                        'six',
-                        'enum34; python_version <= "3.3"',
-                        'futures; python_version < "3"',
-                        'python-dateutil',
+elif sys.version_info[0] == 2:
+    install_requires = ['requests', 'protobuf>=3.4.0', 'six', 'enum34', 'futures', 'python-dateutil',
                         'elasticsearch>=6.0.0,<7.0.0',
-                        'dateparser; python_version >= "2.7"']
+                        'dateparser']
+elif sys.version_info[:2] == (3, 3) or sys.version_info[:2] == (3, 4):
+    install_requires = ['requests', 'protobuf>=3.4.0', 'six', 'python-dateutil', 'elasticsearch>=6.0.0,<7.0.0',
+                        'dateparser']
+elif sys.version_info[0] == 3:
+    install_requires = ['requests', 'protobuf>=3.4.0', 'six', 'enum34', 'python-dateutil', 'elasticsearch>=6.0.0,<7.0.0',
+                        'dateparser']
 
 packages = [
-    'aliyun',
-    'aliyun.log',
-    'aliyun.log.consumer',
-    'aliyun.log.es_migration'
-]
+            'aliyun',
+            'aliyun.log',
+            'aliyun.log.consumer',
+            'aliyun.log.es_migration'
+            ]
 
 version = ''
 with open('aliyun/log/version.py', 'r') as fd:
@@ -52,18 +52,19 @@ with open('aliyun/log/version.py', 'r') as fd:
                         fd.read(), re.MULTILINE).group(1)
 
 classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: Implementation :: PyPy'
-]
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+
 
 long_description = """
 Python SDK for Alicloud Log Service 
@@ -71,13 +72,13 @@ http://aliyun-log-python-sdk.readthedocs.io
 """
 
 setup(
-    name='aliyun-log-python-sdk',
-    version=version,
-    description='Aliyun log service Python client SDK',
-    author='Aliyun',
-    url='https://github.com/aliyun/aliyun-log-python-sdk',
-    install_requires=install_requires,
-    packages=packages,
-    classifiers=classifiers,
-    long_description=long_description
-)
+      name='aliyun-log-python-sdk',
+      version=version,
+      description='Aliyun log service Python client SDK',
+      author='Aliyun',
+      url='https://github.com/aliyun/aliyun-log-python-sdk',
+      install_requires=install_requires,
+      packages=packages,
+      classifiers=classifiers,
+      long_description=long_description
+     )
