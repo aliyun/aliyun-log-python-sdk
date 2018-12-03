017de8506093f4572a8fa63909a6e49133de9255
wjo1212 <wjo1212@163.com>
2018-12-03 23:04:35 +0800

    make setting error more flexible

--- aliyun/log/etl_core/exceptions.py
+++ aliyun/log/etl_core/exceptions.py
@@ -2,7 +2,12 @@ from ..logexception import LogException
 
 
 class SettingError(LogException):
-    def __init__(self, ex=None, settings=""):
-        super(SettingError, self).__init__('InvalidConfig', 'Invalid Settings "{0}"\nDetail: {1}'.format(settings, ex))
+    def __init__(self, ex=None, settings="", msg=""):
+        if msg and settings:
+            msg += 'Invalid Settings "{0}"'.format(settings)
+        else:
+            msg = msg or 'Invalid Settings "{0}"'.format(settings or 'unknown')
+
+        super(SettingError, self).__init__('InvalidConfig', '{0}\nDetail: {1}'.format(msg, ex))
         self.settings = settings
 
