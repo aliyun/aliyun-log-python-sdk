523ed09d14cd92ff32536ea8db712d26340d23b6
wjo1212 <wjo1212@163.com>
2018-12-14 11:18:58 +0800

    move _n to base

--- aliyun/log/etl_core/trans_comp/trans_base.py
+++ aliyun/log/etl_core/trans_comp/trans_base.py
@@ -1,5 +1,24 @@
+import json
+import six
+
+
 class trans_comp_base(object):
-    pass
+    @staticmethod
+    def _n(v):
+        if v is None:
+            return ""
+
+        if isinstance(v, (dict, list)):
+            try:
+                v = json.dumps(v)
+            except Exception:
+                pass
+        elif six.PY2 and isinstance(v, six.text_type):
+            v = v.encode('utf8', "ignore")
+        elif six.PY3 and isinstance(v, six.binary_type):
+            v = v.decode('utf8', "ignore")
+
+        return str(v)
 
 
 class V(trans_comp_base):
