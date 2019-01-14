1dbfabf99fef6f2ebde8fb83ecf486597482f7dc
wjo1212 <wjo1212@163.com>
2019-01-14 10:26:53 +0800

    add py and os version in user agent

--- aliyun/log/version.py
+++ aliyun/log/version.py
@@ -1,5 +1,9 @@
-__version__ = '0.6.41'
-USER_AGENT = 'log-python-sdk-v-' + __version__
+__version__ = '0.6.42'
+
+import sys
+OS_VERSION = str(sys.platform)
+PYTHON_VERSION = str(sys.version_info)
+USER_AGENT = 'log-python-sdk-v-' + __version__ + ", {0}, {1}".format(PYTHON_VERSION, OS_VERSION)
 LOGGING_HANDLER_USER_AGENT = 'logging-handler, ' + USER_AGENT
 ES_MIGRATION_USER_AGENT = 'es-migration, ' + USER_AGENT
 API_VERSION = '0.6.0'
