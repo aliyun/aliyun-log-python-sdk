241f4dacaa729885a1200f437272bbce1bf25835
Qing-Jun Yuan <yuanqj8191@gmail.com>
2020-03-13 10:01:14 +0800

    Update es_migration.__init__.py

--- aliyun/log/es_migration/__init__.py
+++ aliyun/log/es_migration/__init__.py
@@ -1,2 +1,8 @@
-from .migration_manager import MigrationManager
-from .migration_response import MigrationResponse
+#!/usr/bin/env python
+# encoding: utf-8
+
+# Copyright (C) Alibaba Cloud Computing
+# All rights reserved.
+
+
+from .migration_manager import MigrationManager, MigrationConfig
