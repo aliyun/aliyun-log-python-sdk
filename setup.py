071193f21b2cfbc760d3ddbf7edd38c05f71a24f
yuanqj <yuanqj8191@gmail.com>
2022-04-02 11:32:13 +0800

    fix requirements for py2

--- setup.py
+++ setup.py
@@ -21,34 +21,53 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
 import sys
 import re
 
-install_requires_common = ['requests', 'six', 'python-dateutil', 'elasticsearch', 'jmespath']
-install_requires = []
 
+requirements_py3 = [
+    'six',
+    'requests',
+    'python-dateutil',
+    'elasticsearch',
+    'jmespath',
+    'dateparser',
+    'protobuf>=3.4.0',
+]
+requirements_py2 = [
+    'six==1.14.0',
+    'requests==2.23.0',
+    'python-dateutil==2.8.1',
+    'elasticsearch==7.17.2',
+    'jmespath==0.9.5',
+    'enum34==1.1.10',
+    'futures==3.3.0',
+    'protobuf<=3.4.0',
+    'regex==2021.3.17',
+    'tzlocal==2.0.0',
+    'lz4a==0.7.0',
+]
+requirements = []
 if sys.version_info[:2] == (2, 6):
-    install_requires = ['protobuf <= 3.4.0', 'enum34', 'futures']
+    requirements = requirements_py2
 elif sys.version_info[0] == 2:
-    install_requires = ['protobuf <= 3.17.3', 'enum34', 'futures', 'dateparser <= 0.7.6']
+    requirements = requirements_py2 + ['dateparser<=0.7.6']
 elif sys.version_info[:2] == (3, 3):
-    install_requires = ['protobuf>=3.4.0', 'enum34',  'dateparser']
+    requirements = requirements_py3 + ['enum34']
 elif sys.version_info[0] == 3:
-    install_requires = ['protobuf>=3.4.0',  'dateparser']
+    requirements = requirements_py3
 
-install_requires.extend(install_requires_common)
 
 packages = [
-            'aliyun',
-            'aliyun.log',
-            'aliyun.log.ext',
-            'aliyun.log.etl_core',
-            'aliyun.log.etl_core.transform',
-            'aliyun.log.etl_core.trans_comp',
-            'aliyun.log.consumer',
-            'aliyun.log.es_migration'
-            ]
+    'aliyun',
+    'aliyun.log',
+    'aliyun.log.ext',
+    'aliyun.log.etl_core',
+    'aliyun.log.etl_core.transform',
+    'aliyun.log.etl_core.trans_comp',
+    'aliyun.log.consumer',
+    'aliyun.log.es_migration',
+]
 
 version = ''
 with open('aliyun/log/version.py', 'r') as fd:
@@ -56,18 +75,18 @@ with open('aliyun/log/version.py', 'r') as fd:
                         fd.read(), re.MULTILINE).group(1)
 
 classifiers = [
-            'Development Status :: 5 - Production/Stable',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: Implementation :: PyPy'
-    ]
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: Implementation :: PyPy',
+]
 
 
 long_description = """
@@ -76,13 +95,13 @@ http://aliyun-log-python-sdk.readthedocs.io
 """
 
 setup(
-      name='aliyun-log-python-sdk',
-      version=version,
-      description='Aliyun log service Python client SDK',
-      author='Aliyun',
-      url='https://github.com/aliyun/aliyun-log-python-sdk',
-      install_requires=install_requires,
-      packages=packages,
-      classifiers=classifiers,
-      long_description=long_description
-     )
+    name='aliyun-log-python-sdk',
+    version=version,
+    description='Aliyun log service Python client SDK',
+    author='Aliyun',
+    url='https://github.com/aliyun/aliyun-log-python-sdk',
+    install_requires=requirements,
+    packages=packages,
+    classifiers=classifiers,
+    long_description=long_description,
+)
