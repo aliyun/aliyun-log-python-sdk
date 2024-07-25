f347442ac3b24aeb78b08da1d2c0916c9a55e3d3
shuizhao.gh <shuizhao.gh@alibaba-inc.com>
2024-07-25 15:07:20 +0000

    fix: legacy module expt msg

--- aliyun/log/py2_legacy/__init__.py
+++ aliyun/log/py2_legacy/__init__.py
@@ -3,5 +3,7 @@ import sys
 MAX_PYTHON_VERSION = (3, 0)
 
 if sys.version_info >= MAX_PYTHON_VERSION:
-    raise RuntimeError("This package requires only works on Python 2. "
-                       "You are using Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}.")
+    raise RuntimeError("This module works only on Python 2, "
+                       "You are using Python {}.{}.{}".format(sys.version_info.major,
+                                                              sys.version_info.minor,
+                                                              sys.version_info.micro))
