b198d41eab7b3aa9da426ef730295ed0aaf29335
shuizhao.gh <shuizhao.gh@alibaba-inc.com>
2025-03-19 18:29:33 +0800

    handle empty compressed body

--- aliyun/log/compress.py
+++ aliyun/log/compress.py
@@ -3,7 +3,7 @@ import zlib
 from enum import Enum
 from .logexception import LogException
 from .util import Util
-
+import six
 import lz4.block
 
 
@@ -60,12 +60,15 @@ class Compressor():
         compress_type_str = Util.h_v_td(
             header, 'x-log-compresstype', '').lower()
 
-        if compress_type_str == 'lz4':
-            raw_size = int(Util.h_v_t(header, 'x-log-bodyrawsize'))
-            return Compressor.decompress(response, raw_size, CompressType.LZ4)
-
         if compress_type_str == '':
             return response
+
+        raw_size = int(Util.h_v_td(header, 'x-log-bodyrawsize', 0))
+        if raw_size == 0:
+            return six.b('')
+
+        if compress_type_str == 'lz4':
+            return Compressor.decompress(response, raw_size, CompressType.LZ4)
 
         if compress_type_str in ('gzip', 'deflate'):
             return zlib.decompress(response)
